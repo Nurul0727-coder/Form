f4f50a9557fd5fa16ab1a744ad4923b27a9ed2a7
macbook pro <macbookpro@lovie3.local>
2024-12-20 14:36:40 +0800

    added styles

--- src/app/components/StepOne.jsx
+++ src/app/components/StepOne.jsx
@@ -1,14 +1,78 @@
-export const StepOne=({setCurrentStep,onChange})=>{
+// export const StepOne=({setCurrentStep,onChange})=>{
+//     return (
+//         <div className="flex flex-col p-8">
+//             <b className="px-10"> Step 1</b>
+//             <label htmlFor="firstName">FirstName</label>
+//             <input className="border" value={form.firstName}/>
+
+//             <label htmlFor="lastName">LastName</label>
+//             <input className="border" value={form.lastName}/>
+
+//             <button onClick={()=>setCurrentStep(2)}>Next</button>
+//         </div>
+//     )
+// }
+
+import { IoIosArrowForward} from "react-icons/io";
+import{useState, usestate} from'react'
+
+export default function Card ({Next,FormData, handleInputChange}){
+    const[error,setError]=useState('');
+    const handleInputChange=(e)=>{
+        const value=e.tearget.value;
+        const regex=/^[A-Za-z\s]+$/;
+        if(!regex.test(value)){
+            setError('First name cannot contain special charecters or numbers.');
+        }else{
+            setError('');
+        }
+        handleInputChange('firstName',value);
+    };
     return (
-        <div className="flex flex-col p-8">
-            <b className="px-10"> Step 1</b>
-            <label htmlFor="firstName">FirstName</label>
-            <input className="border" value={form.firstName}/>
-
-            <label htmlFor="lastName">LastName</label>
-            <input className="border" value={form.lastName}/>
-
-            <button onClick={()=>setCurrentStep(2)}>Next</button>
+        <div className="w-[480px] h-[655px] bg-white rounded-md flex justify-center relative">
+            <div className="w-[416px] h-[385px] flex-col justify-center mt-8">
+                <div className="w-[416px] h-[130px]">
+                    <img src="Logo.png" className="w-[60px] h-[60px]"/>
+                    <p className="text-[26px] font-semibold">Join Us! 😎</p>
+                    <p className="text-[18px] text-[#8E8E8E]">Please provide all current information accurately.</p>
+                </div>
+                <div className=" w-[416px] h-[230px]  my-[25px] flex-col justify-between">
+                    <div className="w-[416px] h- [68px]">
+                        <p className="font-semibold my-[2px]">First name </p>
+                        <input
+                        value={FormData.firstName}
+                        onChange={handleFirstNameChange}
+                        className=" w-[416px] h-[44px] padding-2 border-[1px] border-[#CBD5E1] rounded-md pl-2"
+                        placeholder="Enter your first name"
+                        />
+                        {error && <p className="text-red-500 text-sm mt-1">{error}</p>}
+                    </div>
+                    <div className="w-[416px] h- [68px]">
+                        <p className="font-semibold my-[2px]">Last name </p>
+                        <input
+                        value={FormData.lastName}
+                        onChange={(e)=> handleInputChange('lastName', e.target.value)}
+                        className=" w-[416px] h-[44px] padding-2 border-[1px] border-[#CBD5E1] rounded-md pl-2"
+                        placeholder="Enter your last name"
+                        />
+                    </div>
+                    <div className="w-[416px] h- [68px] my-[2px]">
+                        <p className="font-semibold my-[2px]"> Username </p>
+                        <input
+                        value={FormData.username}
+                        onChange={(e)=> handleInputChange('username', e.target.value)}
+                        className=" w-[416px] h-[44px] padding-2 border-[1px] border-[#CBD5E1] rounded-md pl-2"
+                        placeholder="Enter your username"
+                        />
+                    </div>
+                </div>
+            </div>
+            <button
+            onClick={Next}
+            className="w-[416px] h-[44] bg-[#D6D8DB] absolute rounded-md my-[580px] text-[#A9ACAF] flex justify-center gap-2 items-center">
+                Continue 1/3
+                <IoIosArrowForward className="w-[10px] h-[10px]"/>
+            </button>
         </div>
     )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,34 +1,84 @@
-'use client';
-import { useState } from "react";
-import{StepOne} from '@/components/StepOne';
-// import{StepTwo} from '@/components/StepTwo';
+// 'use client';
+// import { useState } from "react";
+// import{StepOne} from '@/components/StepOne';
+// // import{StepTwo} from '@/components/StepTwo';
 
 
-const FormBody=(
-{currentStep,setcurrentStep})=>{
+// const FormBody=(
+// {currentStep,setcurrentStep})=>{
 
-  if(currentStep===1){
-    return<StepOne setcurrentStep={setcurrentStep}/>
-  }
+//   if(currentStep===1){
+//     return<StepOne setcurrentStep={setcurrentStep}/>
+//   }
 
-  if(currentStep===2){
-    return<StepTwo setcurrentStep ={setcurrentStep}/>
-  }
+//   if(currentStep===2){
+//     return<StepTwo setcurrentStep ={setcurrentStep}/>
+//   }
 
- return<div>Success</div>;
-}
-export default function Home (){
-  const[currentStep, setcurrentStep]= useState(1);
+//  return<div>Success</div>;
+// }
+// export default function Home (){
+//   const[currentStep, setcurrentStep]= useState(1);
 
-  const[form,setForm]=useState({
-    firstName:'',
+//   const[form,setForm]=useState({
+//     firstName:'',
+//     lastName:'',
+
+//   });
+//   return(
+//     <div className="">
+//       <FormBody currentStep={currentStep}setcurrentStep={setcurrentStep}/>
+//     </div>
+//   )
+// }
+
+"use client"
+
+import Card from './components/stepone'
+import {StepTwo} from './components/steptwo'
+import {StepThree} from './components/stepthree'
+import {StepFour} from './components/stepfour'
+import {use, useState} from 'react'
+
+export default function Home(){
+  const[page,setPage]= useState(1)
+
+  const [formData,setFormData]=useState({
+    firstName:'', 
     lastName:'',
-
+    username:'',
+    email:'',
+    phoneNumber:'',
+    password:'',
+    confirmPassword:'',
+    dob:'',
+    profileImage: null,
   });
-  return(
-    <div className="">
-      <FormBody currentStep={currentStep}setcurrentStep={setcurrentStep}/>
+  
+  const handleInputChange = (field, value)=>{
+    setFormData({
+      ...formData,
+      [field]:value,
+    });
+  }
+
+  const Next=()=>{
+    setPage(page+1)
+  }
+
+  const Back=()=>{
+    setPage(page-1)
+  }
+
+  return (
+    <div className='w-full h-screen flex justify-center items-center bg-[#F4F4F4'>
+      {page===1 && <Card Next={Next} formData={formData} handleInputChange={handleInputChange}/>}
+      {page===2 && <StepTwo Next={Next} formData={formData} handleInputChange={handleInputChange}/>}
+      {page===3 && <StepThree Next={Next} formData={formData} handleInputChange={handleInputChange}/>}
+      {page===4 && <StepFour />}
     </div>
   )
+
 }
+
                                                                
